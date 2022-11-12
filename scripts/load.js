dad99556cc71b7ba328195e90325e778dbab8fc1
carlos delgado <cdelgado@devgenix.be>
2022-11-12 22:53:19 +0100

    Refactor the load script

--- scripts/load.js
+++ scripts/load.js
@@ -1,11 +1,22 @@
+// Define the api url to fetch
+const url = 'https://catfact.ninja/fact';
+
+// Request Option needed by the Fetch call
 let requestOptions = {
     method: 'GET',
     redirect: 'follow'
 };
 
+// Fuction that fetchs the data and update the value in the DOM
 function fetchCatsApi() {
-    fetch("https://catfact.ninja/fact", requestOptions)
-        .then(response => response.text())
-        .then(result => document.getElementById('catFact').innerText = JSON.parse(result).fact)
-        .catch(error => console.log('error', error));
+    // Get the element from the DOM
+    let element = document.getElementById('catFact');
+    // Check that the element exist
+    if (element) {
+        // Fetch the API and parse the response as JSON, catch errors if something fails
+        fetch(url, requestOptions)
+            .then(response => response.text())
+            .then(result => element.innerText = JSON.parse(result).fact)
+            .catch(error => console.log('error', error));
+    }
 }
