2d87cac4650b4c97b676d221c900a499aa7266b6
Carlos Delgado <cdelgado@devgenix.be>
2023-01-10 19:37:31 +0100

    a little refactor :)

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # fetch-example-js-b5
-Fetch an API using JavaScript and Bootstrap 5 for the layout
+Fetch an API using JavaScript and AXIOS. Bootstrap 5 for the layout
 
 # requirements
 Internet connection and JavaScript enabled on the browser
---
+++ scripts/scripts.js
@@ -0,0 +1,21 @@
+async function loadFact() {
+  // Get the element from the DOM
+  let element = document.getElementById('catFact')
+  // check that the element was rendered
+  if (element) {
+    // Consume the API
+    const fact = await fetchCatsApi()
+    element.innerText = fact
+  }
+}
+// Fuction that fetchs the data
+async function fetchCatsApi() {
+  // Define the api url to fetch
+  const url = 'https://catfact.ninja/fact'
+  // Axios GET
+  const response = await axios.get(url)
+  // await the response from the API
+  if (await response.data) {
+    return response.data.fact
+  }
+}
